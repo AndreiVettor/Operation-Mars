675bf96ddb10c519f8fe5f28a4eaf7f65da7b7a7
davidharabagiu <harabagiudavid@gmail.com>
2016-07-16 20:17:26 +0300

    ADDED LAZERSZZZZZZZZZZZ

--- coolgame/LaserGun.cs
+++ coolgame/LaserGun.cs
@@ -12,6 +12,9 @@ namespace coolgame
 {
     public class LaserGun : Entity
     {
+        private List<LaserProjectile> projectiles;
+        private ContentManager content;
+
         public LaserGun(ContentManager content, int x, int y) : base(content)
         {
             SetTexture(content, "laser");
@@ -19,17 +22,31 @@ namespace coolgame
             Height = texture.Height;
             X = x;
             Y = y;
+            projectiles = new List<LaserProjectile>();
+            this.content = content;
         }
 
         public override void Update(GameTime gameTime, InputManager input)
         {
-            Rotation = (float)Math.Atan2(input.MouseY - Y, input.MouseX - X);
+            Rotation = (float)Math.Atan2(input.MouseY - Y - Height / 2, input.MouseX - X - Width / 2);
+
+            if (input.LeftClick)
+            {
+                projectiles.Add(new LaserProjectile(content, X + Width / 2, Y + Height / 2, Rotation));
+            }
+                
+
+            foreach (LaserProjectile p in projectiles)
+                p.Update(gameTime, input);
 
             base.Update(gameTime, input);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
+            foreach (LaserProjectile p in projectiles)
+                p.Draw(spriteBatch);
+
             base.Draw(spriteBatch);
         }
     }
---
+++ coolgame/LaserProjectile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace coolgame
+{
+    public class LaserProjectile : Entity
+    {
+        private float speed = 50f;
+
+        public LaserProjectile(ContentManager content, float x, float y, float direction) : base(content)
+        {
+            SetTexture(content, "redlaser");
+            Width = texture.Width;
+            Height = texture.Height;
+            X = x - Width / 2;
+            Y = y - Height / 2;
+            Rotation = direction;
+        }
+
+        public override void Update(GameTime gameTime, InputManager input)
+        {
+            X += (float)(Math.Cos(Rotation) * speed);
+            Y += (float)(Math.Sin(Rotation) * speed);
+
+            base.Update(gameTime, input);
+        }
+    }
+}
