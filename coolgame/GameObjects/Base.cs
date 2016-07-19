2ec921148471b5e88564a67dbca86aa13c4bed94
davidharabagiu <harabagiudavid@gmail.com>
2016-07-19 17:26:08 +0300

    some adjustments

--- coolgame/GameObjects/Base.cs
+++ coolgame/GameObjects/Base.cs
@@ -21,13 +21,22 @@ namespace coolgame
             Height = texture.Height;
             X = Game.GAME_WIDTH / 2 - Width / 2 - 50;
             Y = groundLevel - Height;
-            EnableHealthBar = true;
             healthBar.MaxHealth = 1000;
             healthBar.Width = 100;
             healthBar.Height = 10;
             layerDepth = LayerManager.GetLayerDepth(Layer.Buildings);
 
             tower = new Tower(content, groundLevel, (int)X);
+
+            collisionBox.X = Math.Min(collisionBox.X, tower.CollisionBox.X);
+            collisionBox.Y = Math.Min(collisionBox.Y, tower.CollisionBox.Y);
+            collisionBox.Width = Math.Max(collisionBox.Width, tower.CollisionBox.Width);
+            collisionBox.Height = Math.Max(collisionBox.Height, tower.CollisionBox.Height);
+
+            EnableHealthBar = true;
+            healthBar.X -= 20;
+            //healthBar.X = collisionBox.X + collisionBox.Width / 2;
+            //healthBar.Y = collisionBox.Y - 20;
         }
 
         public override void Update(float deltaTime, InputManager input)
