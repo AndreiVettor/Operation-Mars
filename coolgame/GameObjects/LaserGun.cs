a752d4cdf2cc09076600440ab318e7cb8e854423
Vettor S. Andrei <kloudrazor@gmail.com>
2016-07-21 23:51:50 +0300

    Fixed Lasergun conflict

--- coolgame/GameObjects/LaserGun.cs
+++ coolgame/GameObjects/LaserGun.cs
@@ -14,58 +14,101 @@ namespace coolgame
     {
         private float cooldownTime;
         private Random random;
+
         private float acceleration;
         private int defaultX;
+        private int recoilOffset;
+        private float recoilAcceleration;
+        private float recoilRecovery;
+
+        private int auxiliaryProjectiles;
+        private float maxSpread;
+        private float cooldown;
+
 
         public LaserGun(ContentManager content, int x, int y) : base(content)
         {
             SetTexture("laser");
             Width = texture.Width;
             Height = texture.Height;
+
             defaultX = x;
+            recoilOffset = 6;
+            recoilAcceleration = -1;
+            recoilRecovery = 0.3f;
+
             X = x;
             Y = y;
             this.content = content;
             layerDepth = LayerManager.GetLayerDepth(Layer.Buildings);
             random = new Random();
+            cooldown = 200f;
         }
 
         public override void Update(float deltaTime)
         {
+            if (InputManager.KeyPress(Keys.D1))
+            {
+                auxiliaryProjectiles++;
+                maxSpread = (float)Math.PI / 40 * auxiliaryProjectiles;
+            }
+            if (InputManager.KeyDown(Keys.D2))
+            {
+                cooldown /= 1.05f;
+
+                recoilRecovery += 0.3f;
+                //recoilOffset -= 1;
+                if (recoilOffset < 0) recoilOffset = 0;
+            }
+
             Rotation = (float)Math.Atan2(InputManager.MouseY - Y - Height / 2, InputManager.MouseX - X - Width / 2);
 
             cooldownTime += deltaTime;
-            if (InputManager.MouseLeft == ButtonState.Pressed && cooldownTime > 200.0f)
+            if (InputManager.MouseLeft == ButtonState.Pressed && cooldownTime > cooldown)
             {
                 SoundManager.PlayClip("laser");
                 cooldownTime = 0;
-                double projectileX = X + Width / 2 +  Math.Cos(Rotation) * (Width / 4);
+                double projectileX = X + Width / 2 + Math.Cos(Rotation) * (Width / 4);
                 double projectileY = Y + Height / 2 + Math.Sin(Rotation) * (Width / 4);
                 LaserProjectile p = new PlayerProjectile(content, projectileX, projectileY, Rotation);
 
                 //Recoil
-                X -= 5;
-                acceleration = -1;
+                X -= recoilOffset;
+                acceleration = recoilAcceleration;
 
-
-                /*for (int i = 0; i < 3; ++i)
+                for (int i = 0; i < auxiliaryProjectiles; ++i)
                 {
-                    p = new PlayerProjectile(content, projectileX, projectileY, Rotation + ((float)random.NextDouble() - .5f) * (float)Math.PI / 20);
-                }*/
+                    p = new PlayerProjectile(content, projectileX, projectileY, Rotation + ((float)random.NextDouble() - .5f) * maxSpread);
+                }
             }
+
             X += acceleration;
-            if (Math.Abs(defaultX - X) < 5)
+
+            //If laser is close enough to default position lock into place
+            if (Math.Abs(defaultX - X) < 2)
             {
                 X = defaultX;
                 acceleration = 0;
             }
 
-            acceleration += 0.2f;
-            if(acceleration > 1)
+            acceleration += recoilRecovery;
+
+            //Clamp acceleration
+            if (acceleration > -recoilAcceleration)
             {
-                acceleration = 1;
+                acceleration = -recoilAcceleration;
             }
 
+            if (Math.Abs(defaultX - X) > 10)
+            {
+                if (defaultX - X > 0)
+                {
+                    X = defaultX - 10;
+                }
+                else {
+                    X = defaultX + 10;
+                }
+            }
 
             base.Update(deltaTime);
         }
