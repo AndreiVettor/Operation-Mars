1e85489a3ab559caa6ee61943002916c30d7a783
Vettor S. Andrei <kloudrazor@gmail.com>
2016-07-28 19:35:39 +0300

    Fixed About Close Button

--- coolgame/GUI/Menus/InformationWindow.cs
+++ coolgame/GUI/Menus/InformationWindow.cs
@@ -8,10 +8,11 @@ namespace coolgame.GUI.Menus
     {
         public InformationWindow(ContentManager Content, string textureName, SpriteFont textFont) : base(Content, textureName)
         {
-            borderPadding = new Vector2(30, 30);
+            borderPadding = new Vector2(0, 0);
+            textPadding = new Vector2(18, 10);
             Center();
-            AddButton(new GUIButton(Content, textFont, "X", new Vector2(0, 0)));
-            //NormalizeButtonLength(false, false, 0);
+            AddButton(new GUIButton(Content, textFont, "x", new Vector2(566, 7),textPadding));
+            TweakButtons(false, true, false, 0);
             SecondaryColor = Color.DarkSlateBlue;
         }
 
