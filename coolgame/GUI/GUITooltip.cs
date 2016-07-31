f1ec96a7fc568d2df81cd8df325c4501d8e2c043
Vettor S. Andrei <kloudrazor@gmail.com>
2016-08-01 01:26:36 +0300

    Fucking multiline Monogame sucks

--- coolgame/GUI/GUITooltip.cs
+++ coolgame/GUI/GUITooltip.cs
@@ -32,28 +32,8 @@ namespace coolgame.GUI
             set { borderColor = value; }
         }
 
-        private List<string> textLines;
-
-        private int lineHeight;
-
-        private int lineSpacing;
-        public int LineSpacing
-        {
-            get { return lineSpacing; }
-            set { lineSpacing = value; }
-        }
-
-        private int currentIndex;
-        private int lastIndex;
-        private string nextString;
-
         public GUITooltip (ContentManager Content, SpriteFont textFont) : base(Content)
         {
-            currentIndex = 0;
-            nextString = "";
-            textLines = new List<string>();
-            lineSpacing = 5;
-
             font = textFont;
             TextCentered = true;
             borderSize = 2;
@@ -70,28 +50,14 @@ namespace coolgame.GUI
 
         public void SetText(string text, int lineNumber)
         {
-            textLines.Clear();
-            currentIndex = 0;
-            nextString = text.Substring(currentIndex, text.Length);
-            for ( int i = 0; i < lineNumber; i++)
-            {
-                lastIndex = currentIndex;
-                currentIndex = nextString.IndexOf("*");
-                if(currentIndex != text.Length - 1)
-                {
-                    nextString = text.Substring(currentIndex, text.Length - 1 - currentIndex);
-                }
-                textLines.Add(text.Substring(lastIndex, currentIndex));
-            }
             this.text = text;
                 
-            Width = (int)font.MeasureString(textLines[0]).X + (int)textPadding.X * 2 + borderSize * 2;
-            Height = (int)font.MeasureString(text).Y * lineNumber + (int)textPadding.Y * 2 + borderSize * 2;
-            lineHeight = (int)font.MeasureString(text).Y;
+            Width = (int)font.MeasureString(text).X + (int)textPadding.X * 2 + borderSize * 2;
+            Height = (int)font.MeasureString(text).Y + (int)textPadding.Y * 2 + borderSize * 2;
 
             if (textCentered)
             {
-                textPosition = new Vector2(Position.X + Width / 2 - font.MeasureString(textLines[0]).X / 2, Position.Y + textPadding.Y + borderSize);
+                textPosition = new Vector2(Position.X + Width / 2 - font.MeasureString(text).X / 2, Position.Y + textPadding.Y + borderSize);
             }
             else
             {
@@ -125,10 +91,8 @@ namespace coolgame.GUI
                             rectangle.Height - borderSize*2),
                         Color.FromNonPremultiplied(backgroundColor.R, backgroundColor.G, backgroundColor.B, (int)BackgroundAlpha));
                 }
-                for(int i = 0; i < textLines.Count; i++) 
-                {
-                    spriteBatch.DrawString(font, textLines[i], textPosition + new Vector2(0, i*(lineHeight + lineSpacing)), Color.FromNonPremultiplied(textColor.R, textColor.G, textColor.B, (int)TextAlpha));
-                }
+
+                    spriteBatch.DrawString(font, Text, textPosition, Color.FromNonPremultiplied(textColor.R, textColor.G, textColor.B, (int)TextAlpha));
             }
         }
     }
